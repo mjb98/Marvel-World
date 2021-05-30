23339db6285b84e0f99707c953a48c2dfd96651f
MJB <mjb1377@gmail.com>
2021-05-30 10:18:09 +0430

    Initial Commit

---
+++ Marvel World/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel World
+//
+//  Created by Mohammad Javad Bashtani on 3/9/1400 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Marvel WorldTests/Marvel_WorldTests.swift
@@ -0,0 +1,33 @@
+//
+//  Marvel_WorldTests.swift
+//  Marvel WorldTests
+//
+//  Created by Mohammad Javad Bashtani on 3/9/1400 AP.
+//
+
+import XCTest
+@testable import Marvel_World
+
+class Marvel_WorldTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
